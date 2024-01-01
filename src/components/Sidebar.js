7aa984d0755f22191d33f0be17e4bb43b9a377b8
Chamuji <s1412052@s.do-johodai.ac.jp>
2024-01-01 13:46:01 +0900

    Sidebarコンポーネント作成

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const Sidebar=()=>{
+    return(
+        <div className="sidebar">
+           <TwitterIcon className="sidebar--twitterIcon"/>
+        </div>
+    );
+}
+
+export default Sidebar;
